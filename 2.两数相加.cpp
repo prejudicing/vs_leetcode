27aff88f8e1cd9907c0d9f73117348052ae252c1
prejudicing <129577736+prejudicing@users.noreply.github.com>
2025-07-04 22:43:50 +0800

    250704

--- 2.两数相加.cpp
+++ 2.两数相加.cpp
@@ -18,6 +18,39 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    ListNode *p1 = l1;
+    ListNode* p2 = l2;
+    ListNode* p3 = new ListNode;
+    ListNode* l3=p3;
+
+    int jinwei = 0;
+    while (!(p1 == nullptr && p2 == nullptr && jinwei==0)) {
+	    ListNode* m = new ListNode;
+        m->val=jinwei;
+	    p3->next = m;
+        int a=0,b=0;
+        if(p1!=nullptr){
+            a=p1->val;
+        }
+        if(p2!=nullptr){
+            b=p2->val;
+        }
+
+	    m->val = m->val + a + b;
+	    jinwei = m->val / 10;
+	    m->val = m->val % 10;
+	    p3 = m;
+        if(p1==nullptr)
+            p1=nullptr;
+        else
+	        p1 = p1->next;
+	    if(p2==nullptr)
+            p2 =nullptr;
+        else
+	        p2 = p2->next;
+    }
+    
+    return l3->next;
         
     }
 };
