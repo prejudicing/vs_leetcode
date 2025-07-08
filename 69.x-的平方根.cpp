34923ced5dc0f24e015964bdbf73c81e6df19d53
prejudicing <129577736+prejudicing@users.noreply.github.com>
2025-07-08 23:53:44 +0800

    “250708”

---
+++ 69.x-的平方根.cpp
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode.cn id=69 lang=cpp
+ *
+ * [69] x 的平方根 
+ *
+ * https://leetcode.cn/problems/sqrtx/description/
+ *
+ * algorithms
+ * Easy (38.78%)
+ * Likes:    1683
+ * Dislikes: 0
+ * Total Accepted:    1.1M
+ * Total Submissions: 2.8M
+ * Testcase Example:  '4'
+ *
+ * 给你一个非负整数 x ，计算并返回 x 的 算术平方根 。
+ * 
+ * 由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 。
+ * 
+ * 注意：不允许使用任何内置指数函数和算符，例如 pow(x, 0.5) 或者 x ** 0.5 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：x = 4
+ * 输出：2
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：x = 8
+ * 输出：2
+ * 解释：8 的算术平方根是 2.82842..., 由于返回类型是整数，小数部分将被舍去。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 <= x <= 2^31 - 1
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int mySqrt(int x) {
+        return sqrt(x);
+    }
+};
+// @lc code=end
+
