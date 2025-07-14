18e330c82cfefc8980e077faea6d80322788c230
prejudicing <129577736+prejudicing@users.noreply.github.com>
2025-07-14 23:46:35 +0800

    250714

---
+++ 59.螺旋矩阵-ii.cpp
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=59 lang=cpp
+ *
+ * [59] 螺旋矩阵 II
+ *
+ * https://leetcode.cn/problems/spiral-matrix-ii/description/
+ *
+ * algorithms
+ * Medium (70.61%)
+ * Likes:    1471
+ * Dislikes: 0
+ * Total Accepted:    558.7K
+ * Total Submissions: 791.7K
+ * Testcase Example:  '3'
+ *
+ * 给你一个正整数 n ，生成一个包含 1 到 n^2 所有元素，且元素按顺时针顺序螺旋排列的 n x n 正方形矩阵 matrix 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：n = 3
+ * 输出：[[1,2,3],[8,9,4],[7,6,5]]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：n = 1
+ * 输出：[[1]]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        
+    }
+};
+// @lc code=end
+
